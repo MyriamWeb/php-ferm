6a5c64fc84f5f7172215e4247b8c5c3daa523868
MyriamWeb <81592812+MyriamWeb@users.noreply.github.com>
2021-07-13 13:17:55 +0200

    4

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <?php include("./includes/ul.inc.html"); ?>
             </nav>
     <section class="m-5 col-8">
-        <!-- <button class="btn btn-primary" type="submit">Ajouter des données</button> -->
+        <button class="btn btn-primary" type="submit">Ajouter des données</button>
         <?php include ("./includes/form.inc.html"); ?>
     </section>
         </div>
