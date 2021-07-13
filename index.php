a01561faa5c9acdfee1bf6f7e4f804f50f9c63b9
MyriamWeb <81592812+MyriamWeb@users.noreply.github.com>
2021-07-13 10:11:51 +0200

    2commit

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Php Procédural</title>
 </head>
 <body>
-    <h1>Coucou</h1>
+
 </body>
 </html>
