80b69b2e0da7ef06c6ba06dba9a8b72fece80d7d
MyriamWeb <81592812+MyriamWeb@users.noreply.github.com>
2021-07-15 16:31:33 +0200

    5

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-
+<!-- ****************INCLUDE PAGE HEAD****************** -->
 <head>
     <?php include("./includes/head.inc.html"); ?> 
 </head>
 
 <body>
+<!-- *********************INCLUDE PAGE HEADER*********************** -->
 <header>
     <?php include("./includes/header.inc.html"); ?> 
 </header>
-
+<!-- **********************NAV**************************** -->
     <div>
         <div class="container-fluid row mx-0">
             <nav class="col-sm-2 my-3">
@@ -25,11 +26,13 @@
                     }
                 ?>
             </nav>
+<!-- *************************SECTION***************************** -->
             <section class=" col-sm-9">
                 <?php
                     if(isset($_GET["add"])){
                         include ("includes/form.inc.html");
                     }
+                    //DECLARATION VARIABLE TABLE
                     elseif (isset($_POST["enregistrer"])){
                         $prénom = $_POST['prénom'];
                         $nom = $_POST['nom'];
@@ -47,12 +50,14 @@
                         $_SESSION["table"] = $table;
                         echo "<h2>Données sauvegardées</h2>";
                     }
+                    //DEBOGAGE
                     elseif(isset($_GET["debugging"])){
                         echo "<h2>Débogage</h2>";
                         print"<pre>";
                         print_r($table);
                         print"</pre>";
                     }
+                    //CONCATENATION
                     elseif(isset($_GET["concatenation"])){
                         echo 
                             "<h2>Concaténation</h2>
@@ -82,6 +87,7 @@
                             "<p>" .$table['age']." ans, je mesure " . $table['size'] .
                             " et je fais partie des " .$table['situation'] . "s de la promo Simplon.</p>";
                     }
+                    //BOUCLE
                     elseif(isset($_GET["loop"])){
                         echo
                             "<h2>Boucle</h2>
@@ -93,6 +99,7 @@
                                 'à la ligne n°' .$i++.' correspond la clé "'.$key. '" et contient ' .$value."<br>"; 
                         }
                     }
+                    //FUNCTION
                     elseif(isset($_GET["function"])){
                         echo
                             "<h2>Fonction</h2>
@@ -100,6 +107,7 @@
                             <p>===> J'utilise ma fonction readTable()</p>";
                         readTable($table);
                     }
+                    //DELETE
                     elseif(isset($_GET["del"])){
                         unset ($_SESSION['table']);
                         echo "<h2>Les données ont bien été supprimées</h2>";
@@ -118,6 +126,7 @@
             </section>
         </div>
     </div>
+<!-- *********************************FOOTER****************************** -->
     <footer>
         <?php include("./includes/footer.inc.html"); ?> 
     </footer>
